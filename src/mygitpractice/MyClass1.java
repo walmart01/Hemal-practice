6d8f2ced85030737383ab3501d8c583c93b732a9
hpurohit01 <hemalpurohit3@gmail.com>
2021-10-29 15:38:52 +0100

    this is my second commit

--- src/mygitpractice/MyClass1.java
+++ src/mygitpractice/MyClass1.java
@@ -3,5 +3,9 @@ package mygitpractice;
 public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("This is first class to go on git");
+
+        NextClassAfterFirstGitSubmit obj = new NextClassAfterFirstGitSubmit();
+        obj.myMethod1();
+
     }
 }
---
+++ src/mygitpractice/NextClassAfterFirstGitSubmit.java
@@ -0,0 +1,8 @@
+package mygitpractice;
+
+public class NextClassAfterFirstGitSubmit {
+
+    void myMethod1(){
+        System.out.println("this is my method 1");
+    }
+}
